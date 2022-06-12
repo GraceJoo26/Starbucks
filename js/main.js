80e52eda3f9dcdb16fdebc9655f4d389db1d1241
gracejoo26 <gracejoo26@gmail.com>
2022-06-12 17:01:37 +0900

    banner

--- js/main.js
+++ js/main.js
@@ -158,6 +158,17 @@ new Swiper('.promotion .swiper-container',{
   }
 });
 
+new Swiper('.awards .swiper-container',{
+  autoplay:true,
+  loop:true,
+  spaceBetween:30,
+  slidesPerView:5,
+  navigation:{
+    prevEl:'.awards .swiper-prev',
+    nextEl:'.awards .swiper-next',
+  }
+});
+
 const promotionEl = document.querySelector ('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;//'숨겨져 있니?'변수에 (false)보이고있다.
